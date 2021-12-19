d456d1d34c1584948100868e32f565bd6a6da217
DongSub_Joung <ehdtjqwjd@kangwon.ac.kr>
2021-12-19 16:58:00 +0900

    before refectoring

---
+++ WebServer(typingAndStudy)/README.md
@@ -0,0 +1,9 @@
+# WebServer in Rust  
+// Just exist "code", not files in cargo EVN(src, cargo files).
+// index.html, 404.html 
+
+# Source  
+- https://www.youtube.com/watch?v=BHxmWTVFWxQ  
+
+# Ref  
+- https://doc.rust-lang.org/std/net/index.html  
--- WebServer(typingAndStudy)/main.rs
+++ WebServer(typingAndStudy)/main.rs
@@ -1,16 +1,40 @@
-// https://www.youtube.com/watch?v=BHxmWTVFWxQ
-
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 
 fn handle_connection(mut stream: TcpStream){
     let mut buffer: [i32; 1024]= [0; 1024];
-
     stream.read(&mut buffer).unwrap();
-    let response: &str= "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+
+    let get: &[u8; 16]= b"GET / HTTP/1.1\r\n";
+    
+    if buffer.starts_with(get){
+        let contents= fs::read_to_string("index.html").unwrap();
+    
+        let response: String= format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+    
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        
+    } else {
+        let status_line: &str= "HTTP/1.1 404 NOT FOUND";
+        let contents: String= 
+            fs::read_to_string("404.html").unwrap();
+        
+        let response: String= format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 fn main(){
