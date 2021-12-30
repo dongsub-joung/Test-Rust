e0998b4b9e20fafe1e686c110106fcae2f28df5f
dongsub-joung <ehdtjqwjd.kangwon.ac.kr>
2021-12-30 12:22:01 +0900

    day 12

---
+++ Practices/src/main.rs
@@ -0,0 +1,6 @@
+use crate PrintEx::StringT::*;
+
+fn main() {
+    let ex= String::from("Hello, world!");
+    new(ex);
+}
---
+++ Practices/target/debug/deps/Practices-2229a64aa7c64f01.d
@@ -0,0 +1,5 @@
+/home/dongsub/git/Test-Rust/Practices/target/debug/deps/Practices-2229a64aa7c64f01: src/main.rs
+
+/home/dongsub/git/Test-Rust/Practices/target/debug/deps/Practices-2229a64aa7c64f01.d: src/main.rs
+
+src/main.rs:
