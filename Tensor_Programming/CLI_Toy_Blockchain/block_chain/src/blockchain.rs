2a37fa6171fac75fd24a3e55af0ee1a0f42ee0dc
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-01-19 19:04:15 +0900

    block Chain cli

--- Tensor_Programming/CLI_Toy_Blockchain/block_chain/src/blockchain.rs
+++ Tensor_Programming/CLI_Toy_Blockchain/block_chain/src/blockchain.rs
@@ -4,7 +4,8 @@ extern crate serde_json;
 extern crate sha2;
 
 use sha2::{ Sha256, Digest };
-use std::fmt::Write;
+use core::slice::SlicePattern;
+use std::{fmt::Write, os::unix::prelude::OsStrExt};
 
 #[derive(Debug, Clone, Serialize)]
 struct Transnaction{
@@ -79,7 +80,94 @@ impl Chain {
 
     pub fn generate_new_block(&mut self) -> bool {
         let header= Blockheader{
-            timestamp: time::now().to_timespec().sec, 
+            timestamp: time::now().to_timespec().sec,
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            merkle: String,
+            difficulty: self.difficulty
         }
+
+        let reward_trans= Transaction{
+            sender: String::from("Root"),
+            reciever: self.moner_addr.clone(),
+            amount: self.reward
+        }
+
+        let mut block= Block{
+            header,
+            count: 0,
+            transactions: vec![]
+        }
+
+        block.transactions.push(reward_trans);
+        block.transactions.append(&mut self.curr_trans);
+        block.count= block.transactions.len() as u32;
+        block.header.merkle= Chain::get_merkle(block.transactions.clone());
+        Cahin::prrof_of_work(&mut block.header);
+
+        println!(":?", &block);
+        self.chain.push(block);
+        true
+    }
+
+    fn get_merkle(curr_trans: Vec<Transaction> -> String){
+        let mut merkle= Vec::new();
+
+        for t in &curr_trans{
+            let hash= Cahin::hash(t);
+            merkle.psuh(hash);
+        }
+        if merkle.len()%2 == 1 {
+            let last= merkle.last().cloned().unwrap();
+            merkle.push(last);
+        }
+        
+        while merkle.len() > 1 {
+            let mut h1= merkle.remove(0);
+            let mut h2= merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh= Chain::hash(&h1);
+            merkle.push(nh);
+        }
+        merkle.pop().unwrap()
+    }
+
+    pub fn proof_of_work(header: &mut Blockheader){
+        loop {
+            let hash= Chain::hash(header);
+            let slice= &hash[..header.defficulty as usize];
+            match slice.parse::<u32>() {
+                Ok(val) => {
+                    if val == 0{
+                        header.nonce += 1;
+                    } else {
+                        println!("Block hash: {}", hash);
+                        break;
+                    }
+                },
+                Err(_) => {
+                    header.nonce += 1;
+                    continue;
+                }
+            }
+        }
+    }
+
+    pub fn hash<T: serde::Serialize>(item: &T) -> string{
+        let input= serde_json::to_string(&item).unwrap();
+        let mut hasher= Sha256::default();
+        hasher.input(input.as_bytes());
+        let res= hasher.result();
+        let vec_res= res.to_vec();
+
+        Chain::hex_to_string(Vec_res.as_slice())
+    }
+
+    pub fn hex_to_string(vec_res: &[u8]) -> String{
+        let mut s= String::new();
+        for b in vec_res{
+            write!(&mut s, "{:x}", b).expect("unable to write");
+        }
+        s
     }
 }
--- Tensor_Programming/CLI_Toy_Blockchain/block_chain/src/main.rs
+++ Tensor_Programming/CLI_Toy_Blockchain/block_chain/src/main.rs
@@ -1,25 +1,107 @@
+#[macro_use]
+extern crate serde_derive;
+
+use std::io;
+use std::process;
+use std::io::Write;
+
 mod blockchain;
 
-fn main() {
-    let Y= String::from("CHAT WITH HER!");
-    let N= String::from("IGNORE HIM!");
-    let mut s= String::new();
-    io::stdin().read_line(&mut s).unwrap();
+fn main(){
+    let mut miner_addr=String::new();
+    let mut difficulty=String::new();
+    let mut choice=String::new();
 
-    if s.len() > 100{
-        panic!("Err");
-    }
-    
-    let mut num= 0u8;
-    let mut cnt= 0u8;
-    for (i,chr) in s.chars().enumerate(){
-        for j in 0..i{
-            if chr == s.as_bytes()[j] as char { num = num+1; }
+    print!("input a miner address");
+    io::stdout().flush();
+    io::stdin().read_line(&mut miner_addr);
+
+    print!("Difficultiy: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut difficulty);
+    let diff= difficulty.trim().parse::<u32>().expect("we need an integer");
+
+    print!("generating genesis blcok! ");
+    let mut chain= blockchain::Chain::new(miner_addr.trim().to_string(), diff);
+
+    loop {
+        println!("Menu");
+        println!("1) New Transaction")
+        println!("2) Mine block")
+        println!("3) Change Difficulty");
+        println!("4) Change Reward");
+        println!("0) Exit");
+        println!("Enter your choice: ");
+
+        io::stdout().flush();
+        choice.clear();
+        io::stdin().read_line(&mut choice);
+        println!("");
+
+        match choice.trim().parse().unwrap(){
+            0 => {
+                println!("exiting!");
+                process::exit(0);
+            },
+            1 => {
+                let mut sender= String::new();
+                let mut receiver= String::new();
+                let mut amount= String::new();
+                
+                println!("enter sender address: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut sender);
+                println!("enter receiver address: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut receiver);
+                println!("enter amount: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut amount);
+
+                let res= chain.new_transaction(sender.trim().to_string(), receiver.trim().to_string(), amount.trim().to_string());
+
+                match res{
+                    true => println!("transaction added"),
+                    false => println!("transaction failed"),
+                }
+            },
+            
+            2 =>{
+                println!("Generating block");
+                let res= chain.generate_new_block();
+                match res {
+                    true => println!("Block generated successfully"),
+                    false => println!("Block generated failed"),
+                }
+            },
+
+            3 => {
+                let mut new_diff= String::new();
+                println!("enter enw difficulty: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut new_diff);
+                chain.update_difficulty(new_diff.trim().parse().unwrap());
+
+                match res {
+                    true => println!("Updated Difficulty"),
+                    false => println!("failed updated Difficulty"),
+                }
+            },
+
+            4 => {
+                let mut new_reward= String::new();
+                print!("Enter new reward: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut new_reward);
+                chain.update_reward(new_reward.trim().parse().unwrap());
+
+                match res {
+                    true => println!("Updated Reward"),
+                    false => println!("failed updated Reward"),
+                }
+            },
+
+            _ => { println!("\t invalid option please retry \t"); },
         }
-        if num == 0 { cnt = cnt+1; }
-        num= 0;
     }
-
-    if cnt%2 == 0 { Y }
-    else { N }
 }
