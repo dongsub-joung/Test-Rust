5c2a940117bbe24f6e8bd42cc77f9cc9bdbeccea
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-01-24 18:38:36 +0900

    manual adding

--- programmers.kr/lv1/12906_lib.rs
+++ programmers.kr/lv1/12906_lib.rs
@@ -25,21 +25,45 @@ fn solution2(list: Box<[usize]>) -> Vec<usize>{
     result
 }
 
+fn solution3(list: Box<[usize]>) -> Vec<usize>{
+    let mut vs= Vec::new();
+    let mut temp=0usize;
+    for i in list.iter(){
+        if temp == *i {
+            vs.push(temp);
+        }
+        temp= *i;
+    }
+
+    let mut temp2= 0usize;
+    let mut vss: Vec<usize>= Vec::new();
+    for element in vs.iter(){
+        if temp2 != *element{
+            vss.push(*element);
+        } else{
+            continue;
+        }
+        temp2= *element;
+    }    
+
+    vss
+}
+
 mod testing{
     use super::*;
 
     #[test]
     fn test1(){
         let input= Box::new([1,1,3,3,0,1,1]);
-        let result= Box::new([1,3,0,1]);
-        assert_eq!(solution(input), result);
+        let result= Vec::from([1,3,0,1]);
+        assert_eq!(solution3(input), result);
     }
 
     #[test]
     fn test2(){
         let input= Box::new([4,4,4,3,3]);
-        let result= Box::new([4,3]);
-        assert_eq!(solution(input), result);
+        let result= Vec::from([4,3]);
+        assert_eq!(solution3(input), result);
     }
 
 }
