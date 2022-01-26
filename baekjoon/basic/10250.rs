3a18204fe861e37dd8021660ef758df50ef918bb
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-01-26 14:39:25 +0900

    math 1

---
+++ baekjoon/basic/10250.rs
@@ -0,0 +1,45 @@
+use std::{i128};
+
+struct Data{
+    h: i128,
+    w: i128,
+    n: i128
+}
+impl Data {
+    fn new(h: i128, w: i128, n: i128) -> Data{
+        Data{
+            h,w,n
+        }
+    }
+
+    fn calculation(data: Data) -> String{
+        let num= data.n / data.h + 1;
+        let floor= data.n % data.h;
+        let result= floor*100 + num;
+        if data.n % data.h == 0{
+            let num= data.n / data.h + 1;
+            let floor= data.h;
+            let result= floor *100 + num;
+            
+            return result.to_string();
+        }
+        return result.to_string();
+    }
+}
+
+fn main() {
+    // input value
+    let t= 1i128;
+    let mut v : Vec<String>= Vec::new();
+    for _ in 0..t{
+        // input value
+        // let (h, w, n)= (6i128, 12i128, 10i128); 
+        let (h, w, n)= (30, 50, 72); 
+        let data= Data::new(h,w,n);
+        v.push(Data::calculation(data)); 
+    }
+
+    for i in v.iter(){
+        println!("{}", i);
+    }
+}
