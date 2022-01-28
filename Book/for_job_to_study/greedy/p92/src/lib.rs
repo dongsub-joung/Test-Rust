29a768c1cf55690b844f576a09c7a6c878f84d43
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-01-28 23:51:47 +0900

    ch 2

---
+++ Book/for_job_to_study/greedy/p92/src/lib.rs
@@ -0,0 +1,45 @@
+#[cfg(test)]
+
+fn solution(first: &str, second: &str) -> usize{
+    let mut result= 0_usize;
+
+    let mut f_list= first.split_whitespace();
+    let n= f_list.next().unwrap().parse::<usize>().unwrap();
+    let mut m= f_list.next().unwrap().parse::<usize>().unwrap();
+    let k= f_list.next().unwrap().parse::<usize>().unwrap();
+
+    let mut data: Vec<usize>= Vec::new();
+    let s_list= second.split_whitespace();
+    for i in s_list{
+        let val= i.parse().unwrap();
+        data.push(val);
+    }
+
+    data.sort();
+
+    let max= data[n-1];
+    let second= data[n-2];
+
+    loop {
+        for i in 0..k{
+            if m == 0 { break; }
+            result += max;
+            m -= 1;
+        }
+
+        if m == 0{ break; }
+        result += second;
+        m -= 1;
+    }
+
+    result
+}
+
+mod test{
+    use super::*;
+    
+    #[test]
+    fn test1(){
+        assert_eq!(solution("5 8 3", "2 4 5 4 6"), 46);
+    }
+}
