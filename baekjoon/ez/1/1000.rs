a689bea7f85312f946e5449fff3ef942f4225b7d
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-03-12 10:53:47 +0900

    1000.rs

---
+++ baekjoon/ez/1/1000.rs
@@ -0,0 +1,21 @@
+use std::io::{self, stdin};
+
+fn main(){
+    let (a,b)= inputing();
+    if a > 10 && b > 10 {
+        panic!("Err");
+    }
+
+    let result= a+b;
+    println!("{}", result);
+}
+
+fn inputing() -> (usize, usize) {
+    let mut buf= String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let mut list= buf.split_whitespace();
+    let a: usize= list.next().unwrap().parse().unwrap();
+    let b: usize= list.next().unwrap().parse().unwrap(); 
+
+    (a,b)
+}
