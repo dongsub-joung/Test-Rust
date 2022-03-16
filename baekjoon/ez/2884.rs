e74f67f1b42cdf65a1f7c6107db1519dccc837a5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-03-16 13:48:08 +0900

    2884

---
+++ baekjoon/ez/2884.rs
@@ -0,0 +1,23 @@
+use std::io::{self, stdin};
+
+fn main(){
+    let (mut h,mut m)= inputing();
+
+    let r= m - 45;
+    if r < 0 {
+        h -= 1;
+        m = 60 + r;
+    }
+
+    println!("{} {}", h, m)
+}
+
+fn inputing()->(usize, i32){
+    let mut buf= String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let mut list= buf.split_whitespace();
+    let h: usize= list.next().unwrap().trim().parse().unwrap();
+    let m: i32= list.next().unwrap().trim().parse().unwrap();
+
+    (h, m)
+}
