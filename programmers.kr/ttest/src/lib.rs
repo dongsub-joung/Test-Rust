f4d6e4a5fac2071d8295dcecc28f275f3c83223a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-09 19:36:16 +0900

    s

--- programmers.kr/ttest/src/lib.rs
+++ programmers.kr/ttest/src/lib.rs
@@ -1,50 +1,48 @@
 use std::{io::{self, stdin}};
 
 #[cfg(test)]
-fn solution(mut board: [[usize; 5]; 5], moves: Vec<usize>) -> i32{
-    // let stack_list= Box::new([]);
-    let mut stack_list: Vec<&mut usize>= Vec::new();
-    let mut answer= 0;
+fn solution(id_list: Vec<&str>, report: Vec<&str>, k: usize) -> Vec<usize>{
+    let size= id_list.len();
+    let mut result= vec![0; size];
+    let check: Vec<(usize, usize)>= Vec::new();
 
-    for i in moves.iter(){
-        let mut board= board.clone();
-        for j in 0..board.len(){
-            if board[j][i-1] != 0{
-                // Two mutable value complie trouble
-                stack_list.push(&mut board[j][i-1]);
-                board[j][i-1]= 0;
-
-                if stack_list.len() > 1 {
-                    // let copy= stack_list
-                    if  stack_list.pop().unwrap() == stack_list.pop().unwrap(){
-                        // index -1
-                        // stack_list.pop();
-                        // stack_list.pop();
-                        answer += 2;
-                    }
-                }
-                break;
+    let mut p= vec![0; size];
+    let mut point: Vec<i32> = Vec::new();
+    for _ in 0..size{
+        point.push(p);
+    }
+    
+    for one in report{
+        let mut one= one.split_whitespace();
+        let send= one.next().unwrap();
+        let receive= one.next().unwrap();
+        for (i, name) in id_list.iter().enumerate(){
+            if name == receive{
+                point[]
             }
-        }
+        }        
     }
 
-    answer
+    result
 }
+
 mod testing{
     use super::*;
 
     #[test]
     fn test1(){
-        let board = [
-            [0,0,0,0,0],
-            [0,0,1,0,3],
-            [0,2,5,0,1],
-            [4,2,4,4,2],
-            [3,5,1,3,1]
-            ];
-        let moves = [1,5,3,5,1,2,1,4].to_vec();
-        let result= 4;
-        assert_eq!(solution(board, moves), result);
+        let id_list= vec!["muzi", "frodo", "apeach", "neo"];
+        let report= vec!["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"];
+        let k= 2;
+        let result= vec![2,1,1,0];
+        assert_eq!(solution(id_list, report, k), result);
     }
 
+    fn test2(){
+        let id_list= vec!["con", "ryan"];
+        let report= vec!["ryan con", "ryan con", "ryan con", "ryan con"];
+        let k= 3;
+        let result= vec![0,0];
+        assert_eq!(solution(id_list, report, k), result);
+    }
 }
