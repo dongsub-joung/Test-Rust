373c0c819006a7a76b958fa8ecbdbcc30dd33977
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-10 09:21:42 +0900

    thinking

--- programmers.kr/ttest/src/lib.rs
+++ programmers.kr/ttest/src/lib.rs
@@ -6,21 +6,41 @@ fn solution(id_list: Vec<&str>, report: Vec<&str>, k: usize) -> Vec<usize>{
     let mut result= vec![0; size];
     let check: Vec<(usize, usize)>= Vec::new();
 
-    let mut p= vec![0; size];
-    let mut point: Vec<i32> = Vec::new();
+    let size= 5_usize;
+    let mut p= vec![0_usize; size];
+    let mut point: Vec<Vec<usize>> = Vec::new();
     for _ in 0..size{
-        point.push(p);
+        point.push(p.clone());
     }
     
+    let (mut a, mut b)= (0usize, 0usize);
     for one in report{
         let mut one= one.split_whitespace();
         let send= one.next().unwrap();
         let receive= one.next().unwrap();
-        for (i, name) in id_list.iter().enumerate(){
-            if name == receive{
-                point[]
+
+        for (j, name) in id_list.iter().enumerate(){
+            if name == &send{
+                a= j;
             }
-        }        
+        }
+        for (j, name) in id_list.iter().enumerate(){
+            if name == &receive{
+                b= j;
+            }
+        }
+        point[a][b] += 1;        
+    }
+
+    for i in 0..size{
+        let mut sum= 0usize;
+        for j in 0..size{
+            if point[i][j] > 1{
+                point[i][j] = 1;
+            }
+            sum += point[i][j];
+        }
+        result.push(sum);
     }
 
     result
@@ -38,6 +58,7 @@ mod testing{
         assert_eq!(solution(id_list, report, k), result);
     }
 
+    #[test]
     fn test2(){
         let id_list= vec!["con", "ryan"];
         let report= vec!["ryan con", "ryan con", "ryan con", "ryan con"];
