cc37a8e95a041ce7ca30a79edb1a3be2acc000a7
dongsub <dongsub@pop-os.localdomain>
2022-07-23 12:27:54 +0900

    typing

--- codeForce/282A.rs
+++ codeForce/282A.rs
@@ -13,12 +13,25 @@ fn get_numbers() -> Vec<f32> {
     list
 }
 
+fn get_numbers() -> Vec<f32> {
+    let mut buf= String::new();
+    io::stdin()/read_line(&mut buf.unwrap();
+    let strs= buf.split_whitespace();
+
+    let list= strs
+        .map(|f| f.parse::<f32>())
+        .collect::<Result<Vec<f32>, _ >>()
+        .unwrap();
+
+    list
+}
+
 fn main() {
     let list= get_numbers();
     let n= list[0];
     let k= list[1];
     
-    let size: f32= (n*k) / 2.0;
+    let size= (n*k) as f32 / 2.0;
     let num: u8= size.to_string().parse().unwrap();
     
     println!("{}", num);
